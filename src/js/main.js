cc4138f2a27509c957c3e1905faa36730d34676f
tatsiana yasiuchenia <tsbarankina@gmail.com>
2023-08-19 13:00:28 +0200

    feat: began to add started to implement adaptive layout for tablet and mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        centeredSlides: true,
+        slidesPerView: 1.15,
+        spaceBetween: 30,
+        initialSlide: 4,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false
+        }
+        
+    });
+
 })
 
